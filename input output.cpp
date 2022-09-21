3e3df945830b5f960ca3701bd5bdc1030977defe
briskgaurav <114090891+briskgaurav@users.noreply.github.com>
2022-09-21 21:14:29 +0530

    DSA Assignment 1

---
+++ input output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   int a=1,b=2,c=7;
+cout<<"enter the vale of a";
+ cin>>a;
+ cout<<"enter the value of b";
+ cin>>b;
+ cout<<"enter the vale of c";
+ cin>>b;
+ cout<<"the sum is :"<<a+c+c;
+      
+    return 0;
+}
